15756dcc062e650b0860c9ade6fcf689ec1a2fb4
Tuong Khang <nteeka2002@gmail.com>
2023-02-28 15:10:52 +0700

    lab03

---
+++ Lab03_NguyenTuongKhang/Lab03_NguyenTuongKhang/Controllers/SachController.cs
@@ -0,0 +1,88 @@
+﻿using Lab03_NguyenTuongKhang.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab03_NguyenTuongKhang.Controllers
+{
+    public class SachController : Controller
+    {
+        // GET: Sach
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult Index()
+        {
+            var all_sach = from tt in data.Saches select tt;
+            return View(all_sach);
+        }
+        public ActionResult Detail(int id)
+        {
+            var D_sach = data.Saches.Where(m => m.masach == id).First();
+            return View(D_sach);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, Sach sach)
+        {
+            var ten = collection["tensach"];
+           
+            var hinh = collection["hinh"];
+            if (string.IsNullOrEmpty(ten))
+            {
+                ViewData["Error"] = "Don't Empty";
+            }
+            else
+            {
+                
+                sach.tensach = ten;
+               
+                sach.hinh = hinh;
+                data.Saches.InsertOnSubmit(sach);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Create();
+        }
+        public ActionResult Edit(int? id)
+        {
+            var E_category = data.Saches.First(m => m.masach == id);
+            return View(E_category);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            var sach = data.Saches.First(m => m.masach == id);
+            var E_tenloai = collection["tensach"];
+            sach.maloai = id;
+            if (string.IsNullOrEmpty(E_tenloai))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                sach.tensach = E_tenloai;
+                UpdateModel(sach);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Edit(id);
+        }
+        public ActionResult Delete(int id)
+        {
+            var D_sach = data.Saches.First(m => m.masach == id);
+            return View(D_sach);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            var D_sach = data.Saches.Where(m => m.masach == id).First();
+            data.Saches.DeleteOnSubmit(D_sach);
+            data.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Lab03_NguyenTuongKhang/Lab03_NguyenTuongKhang/Controllers/TheLoaiController.cs
@@ -0,0 +1,83 @@
+﻿using Lab03_NguyenTuongKhang.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab03_NguyenTuongKhang.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        // GET: TheLoai
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult Index()
+        {
+            var all_theloai = from tt in data.TheLoais select tt;
+            return View(all_theloai);
+        }
+        public ActionResult Detail(int id)
+        {
+            var D_theloai = data.TheLoais.Where(m => m.maloai == id).First();
+            return View(D_theloai);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, TheLoai tl)
+        {
+            var ten = collection["tenloai"];
+            if (string.IsNullOrEmpty(ten))
+            {
+                ViewData["Error"] = "Don't Empty";
+            }
+            else
+            {
+                tl.tenloai = ten;
+                data.TheLoais.InsertOnSubmit(tl);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Create();
+        }
+        public ActionResult Edit(int id)
+        {
+            var E_category = data.TheLoais.First(m => m.maloai == id);
+            return View(E_category);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            var theloai = data.TheLoais.First(m => m.maloai == id);
+            var E_tenloai = collection["tenloai"];
+            theloai.maloai = id;
+            if (string.IsNullOrEmpty(E_tenloai))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                theloai.tenloai = E_tenloai;
+                UpdateModel(theloai);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Edit(id);
+        }
+        public ActionResult Delete(int id)
+        {
+            var D_theloai = data.TheLoais.First(m => m.maloai == id);
+            return View(D_theloai);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            var D_theloai = data.TheLoais.Where(m => m.maloai == id).First();
+            data.TheLoais.DeleteOnSubmit(D_theloai);
+            data.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
